794f6fa59b8ffab1dd4fb84fa4e8d3687787bb3e
jathi <siva.kaddala1998@gmail.com>
2020-02-10 22:50:53 +0530

    basic

---
+++ calci/src/main/java/simple/calci/Addition.java
@@ -0,0 +1,14 @@
+package simple.calci;
+
+public class Addition {
+	double x,y;
+	public Addition(double x,double y) {
+		// TODO Auto-generated constructor stub
+		this.x=x;
+		this.y=y;
+	}
+	//method
+	void add() {
+		System.out.println(this.x+this.y);
+	}
+}
---
+++ calci/src/main/java/simple/calci/Subtraction.java
@@ -0,0 +1,14 @@
+package simple.calci;
+
+public class Subtraction {
+	double x,y;
+	public Subtraction(double x,double y) {
+		// TODO Auto-generated constructor stub
+		this.x=x;
+		this.y=y;
+	}
+	void sub() {
+		System.out.println(this.x-this.y);
+	}
+
+}
